ddf32e6fee5447cc2fcf630023d17611ce8b666a
tasib94 <rahi84974@gmail.com>
2025-06-05 23:15:06 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-lime-300'>Vite + React</h1>
+      <button class="btn w-64 rounded-full text-amber-500 bg-blue-700">Button</button>
+    
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form } from 'react-router';
+
+
+
+
+const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(email, password)
+    }
+
+
+
+
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-3xl font-bold">Login now!</h1>
+
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <Form onSubmit={handleLogin} className="fieldset">
+                            <label className="label">Email</label>
+                            <input type="email" name="email" className="input" placeholder="Email" />
+                            <label className="label">Password</label>
+                            <input type="password" name="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-secondary mt-4">Login</button>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
